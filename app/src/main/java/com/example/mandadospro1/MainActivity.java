d921f34673d77f6b19f1f872a6e9719ec668545f
fernando <mi476104@uaeh.edu.mx>
2023-08-16 18:40:06 -0500

    Login base

--- app/src/main/java/com/example/mandadospro1/MainActivity.java
+++ app/src/main/java/com/example/mandadospro1/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.mandadospro1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Entrar(View view){
+
+        /*Intent p1 = new Intent(this, P1.class);
+        if(et1.getText().toString().matches("")){
+
+            Toast.makeText(this, "Ingresa un nombre", Toast.LENGTH_SHORT).show();
+
+        }else{
+
+            p1.putExtra("user", et1.getText().toString());
+            startActivity(p1);*/
+
+        }
+
+    }
+
 }
